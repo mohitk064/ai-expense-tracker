ade88e728630e0e73355a99e20c5073e64bac1ad
Mohit <mohitranjank064@gmail.com>
2026-07-15 01:14:47 +0530

    Set up JPA and PostgreSQL integration

---
+++ backend/src/main/java/com/mohit/expensetracker/dto/RegisterRequest.java
@@ -0,0 +1,14 @@
+package com.mohit.expensetracker.dto;
+
+import java.time.LocalDate;
+
+public class RegisterRequest {
+
+  private String name;
+  private String email;
+  private String password;
+  private String confirmPassword;
+  private String phoneNumber;
+  private String dateOfBirth;
+  
+}
